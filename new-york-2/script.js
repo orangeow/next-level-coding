9faad361089d830f9f9000744aa86c939b3672a4
orangeow <143812812+orangeow@users.noreply.github.com>
2024-05-09 14:15:52 -0400

    Add files via upload

--- new-york-2/script.js
+++ new-york-2/script.js
@@ -3,16 +3,20 @@
 let wallContainer = document.getElementById("wall-container");
 
 for(let i = 0; i<200;i++){
-    wallContainer.innerHTML += "<div class='tile-set'> <div class='tile'></div> <div class='tile'></div> <div class='tile'></div> <div class='tile'></div> <div class='tile'></div> <div class='tile'></div> <div class='tile'></div> <div class='tile'></div> <div class='tile'></div></div>";
+    wallContainer.innerHTML += "<div class='tile-set'> <div class='tile'></div> <div class='tile'></div> <div class='tile'></div> <div class='tile'></div> <div class='tile'></div> <div class='tile'></div> <div class='tile'></div> <div class='tile'></div> <div class='tile'></div>";
 }
 
 let tileSet = document.getElementsByClassName("tile-set");
 let tile = document.getElementsByClassName("tile");
 
+let chip = "<div class='chip'></div>";
+
+let key =  "<div class='key'></div> </div>";
+
 //color palettes
-let nycPalette = ['rgb(89, 182, 140)', 'rgb(241, 219, 59)','rgb(135, 183, 66)'];//dark,light,medium
+let nycPalette = ['rgb(89, 182, 140)', 'rgb(135, 183, 66)','rgb(241, 219, 59)'];//dark,medium,light
 let usaPalette = ['rgb(196, 30, 61)','rgb(223, 90, 33)','rgb(236, 164, 0)'];//dark, medium, light
-let worldPalette = ['rgb(25, 57, 109)','rgb(118, 146, 255)','rgb(30, 90, 186)']//dark,light,medium
+let worldPalette = ['rgb(25, 57, 109)','rgb(30, 90, 186)','rgb(118, 146, 255)']//dark,medium,light
 
 //questions and answers
 let q1Options = document.getElementsByClassName("q1-option");
@@ -34,7 +38,7 @@ let allPositions = [];
 let allQ1Answers = [];
 let allQ2Answers = [];
 let allQ3Answers = [];
-let allGroutColors = [];
+let allChips = [];
 
 ////checking cookies
 
@@ -46,28 +50,71 @@ if(checkCookie("positions")){
     allQ1Answers = JSON.parse(getCookie("q1Answers"));
     allQ2Answers = JSON.parse(getCookie("q2Answers"));
     allQ3Answers = JSON.parse(getCookie("q3Answers"));
-    allGroutColors = JSON.parse(getCookie("groutColors"));
+    allChips = JSON.parse(getCookie("chips"));
 
     for(let i=0;i<allPositions.length;i++){//scrolling through the listed positions which will be assigned to elements of the tileSet
         // for(let j=0;j<allColors.length;j++){//scrolling through the color sets stored in the cookie
             for(let k=0;k<9;k++){//scrolling through the colors within each color set
                 tileSet[allPositions[i]].children[k].style.backgroundColor = allColors[i][k];//it is replacing the colors of the previous tiles with the last one, need to redo the way these for loops are organized. I think it unforunately matters how they are stacked up
                 // console.log(allColors[j][k]);
-                console.log(document.cookie);
             }
         // }
     }
 
     for(let i =0;i<allPositions.length;i++){
-        for(let j=0;j<9;j++){
-            tileSet[allPositions[i]].children[j].style.border = "1px solid " + allGroutColors[i];
-        }
+        tileSet[allPositions[i]].innerHTML += allChips[i];
+        // for(let j=0;j<9;j++){
+            // tileSet[allPositions[i]].children[j].style.border = "1px solid " + allChips[i];
+        // }
     }
-
+    for(let i = 0;i<tileSet.length;i++){
+        tileSet[i].innerHTML += key;
+    }
 } else{
     console.log("no cookies here")
+    for(let i = 0;i<tileSet.length;i++){
+        tileSet[i].innerHTML += key;
+    }
 }
 
+let keys = document.getElementsByClassName("key");
+
+//"From " + q2Answer + "<br>Lives in NYC";
+
+for(let i=0;i<allPositions.length;i++){
+    if(allQ1Answers[i] == "nyc"){
+        if(allQ3Answers[i] == "Yes"){
+            keys[allPositions[i]].innerHTML = "From " + allQ2Answers[i] + "<br>Lives in NYC";
+        } else if(allQ3Answers[i] =="No"){
+            keys[allPositions[i]].innerHTML = "From " + allQ2Answers[i] + "<br>Visiting NYC";
+        }
+    } else if (allQ1Answers[i] == "otherUS"){
+        if(allQ3Answers[i] == "Yes"){
+            keys[allPositions[i]].innerHTML = "From " + allQ2Answers[i] + " of the US<br>Lives in NYC";
+        } else if(allQ3Answers[i] =="No"){
+            keys[allPositions[i]].innerHTML = "From " + allQ2Answers[i] + " of the US<br>Visiting NYC";
+        }
+    } else if(allQ1Answers[i] == "international"){
+        if(allQ3Answers[i] == "Yes"){
+            keys[allPositions[i]].innerHTML = "From " + allQ2Answers[i] + "<br>Lives in NYC";
+        } else if(allQ3Answers[i] =="No"){
+            keys[allPositions[i]].innerHTML = "From " + allQ2Answers[i] + "<br>Visiting NYC";
+        }
+    }
+}
+
+//adding hovers to keys
+
+for(let i=0;i<tileSet.length;i++){
+    if(tileSet[i].children[0].style.backgroundColor != ""){
+        keys[i].addEventListener("mouseover",function(){
+            keys[i].style.opacity = "70%";
+        })
+        keys[i].addEventListener("mouseout",function(){
+            keys[i].style.opacity = "0";
+        })
+    }
+}
 
 //making the object that is going to get stored
 let q1Answer;
@@ -75,7 +122,7 @@ let q2Answer;
 let q3Answer;
 let finalPosition;
 let colorSet = [];
-let groutColor;
+// let ;
 
 //open menu
 let selectionMenu = document.getElementById("selection-menu");
@@ -129,13 +176,14 @@ let submitButton = document.getElementById("submit");
 function submit(){
 
     //random position that doesn't already have a tile on it
-    let randomTileSet = Math.floor(Math.random()*tileSet.length);
-    for(let i=0;i<tileSet.length;i++){
-        if((i%tileSet.length == randomTileSet) && (tileSet[i].children[0].style.backgroundColor == "") && !((randomTileSet >= 43 && randomTileSet <= 49) || (randomTileSet >=63 && randomTileSet <=69) || (randomTileSet >=73 && randomTileSet <= 79))){
+    let randomTileSet = Math.floor(Math.random()*1000);
+    for(let i=0;i<1000;i++){
+        if((i == randomTileSet) && (tileSet[i%tileSet.length].children[0].style.backgroundColor == "") && !((randomTileSet%tileSet.length >= 43 && randomTileSet%tileSet.lengtht <= 49) || (randomTileSet%tileSet.length >=63 && randomTileSet%tileSet.length <=69) || (randomTileSet%tileSet.length >=73 && randomTileSet%tileSet.length <= 79))){
             finalPosition = i%tileSet.length;
             console.log(finalPosition);
             break;
-        } else {
+        } else if((i == randomTileSet) && (tileSet[i%tileSet.length].children[0].style.backgroundColor != "")){
+            randomTileSet = Math.floor(Math.random()*1000);
             continue;
         }
     }
@@ -147,69 +195,69 @@ function submit(){
                             nycPalette[1], nycPalette[0], nycPalette[1], 
                             nycPalette[2], nycPalette[1], nycPalette[2]];
             } else if (q2Answer == "Brooklyn"){
-                colorSet = [nycPalette[1], nycPalette[1], nycPalette[2], 
-                            nycPalette[1], nycPalette[0], nycPalette[1], 
-                            nycPalette[2], nycPalette[1], nycPalette[1]];
-            } else if (q2Answer == "Queens"){
                 colorSet = [nycPalette[2], nycPalette[2], nycPalette[2], 
-                            nycPalette[1], nycPalette[1], nycPalette[1], 
-                            nycPalette[0], nycPalette[0], nycPalette[0]];
+                            nycPalette[2], nycPalette[1], nycPalette[1], 
+                            nycPalette[2], nycPalette[1], nycPalette[0]];
+            } else if (q2Answer == "Queens"){
+                colorSet = [nycPalette[2], nycPalette[1], nycPalette[1], 
+                            nycPalette[2], nycPalette[1], nycPalette[0], 
+                            nycPalette[2], nycPalette[1], nycPalette[1]];
             } else if (q2Answer == "Bronx"){
-                colorSet = [nycPalette[0], nycPalette[1], nycPalette[2], 
-                            nycPalette[1], nycPalette[1], nycPalette[2], 
+                colorSet = [nycPalette[2], nycPalette[1], nycPalette[0], 
+                            nycPalette[2], nycPalette[1], nycPalette[1], 
                             nycPalette[2], nycPalette[2], nycPalette[2]];
             } else if (q2Answer == "Staten Island"){
-                colorSet = [nycPalette[1], nycPalette[1], nycPalette[0], 
-                            nycPalette[1], nycPalette[0], nycPalette[2], 
-                            nycPalette[0], nycPalette[2], nycPalette[2]];
+                colorSet = [nycPalette[2], nycPalette[2], nycPalette[2], 
+                            nycPalette[1], nycPalette[1], nycPalette[2], 
+                            nycPalette[0], nycPalette[1], nycPalette[2]];
             }
         } else if(q1Answer == "otherUS"){
             if(q2Answer == "West"){
-                colorSet = [usaPalette[0], usaPalette[1], usaPalette[0], 
-                            usaPalette[1], usaPalette[2], usaPalette[1], 
-                            usaPalette[0], usaPalette[1], usaPalette[0]];
+                colorSet = [usaPalette[1], usaPalette[1], usaPalette[2], 
+                            usaPalette[0], usaPalette[1], usaPalette[2], 
+                            usaPalette[1], usaPalette[1], usaPalette[2]];
             } else if (q2Answer == "Southwest"){
-                colorSet = [usaPalette[1], usaPalette[1], usaPalette[0], 
-                            usaPalette[1], usaPalette[2], usaPalette[1], 
-                            usaPalette[0], usaPalette[1], usaPalette[1]];
-            } else if (q2Answer == "Midwest"){
                 colorSet = [usaPalette[2], usaPalette[2], usaPalette[2], 
-                            usaPalette[1], usaPalette[1], usaPalette[1], 
-                            usaPalette[0], usaPalette[0], usaPalette[0]];
+                            usaPalette[1], usaPalette[1], usaPalette[2], 
+                            usaPalette[0], usaPalette[1], usaPalette[2]];
+            } else if (q2Answer == "Midwest"){
+                colorSet = [usaPalette[2], usaPalette[1], usaPalette[2], 
+                            usaPalette[1], usaPalette[0], usaPalette[1], 
+                            usaPalette[2], usaPalette[1], usaPalette[2]];
             } else if (q2Answer == "Southeast"){
-                colorSet = [usaPalette[2], usaPalette[1], usaPalette[0], 
-                            usaPalette[1], usaPalette[1], usaPalette[0], 
-                            usaPalette[0], usaPalette[0], usaPalette[0]];
+                colorSet = [usaPalette[2], usaPalette[2], usaPalette[2], 
+                            usaPalette[2], usaPalette[1], usaPalette[1], 
+                            usaPalette[2], usaPalette[1], usaPalette[0]];
             } else if (q2Answer == "Northeast"){
-                colorSet = [usaPalette[0], usaPalette[0], usaPalette[2], 
-                            usaPalette[0], usaPalette[2], usaPalette[1], 
-                            usaPalette[2], usaPalette[1], usaPalette[1]];
+                colorSet = [usaPalette[2], usaPalette[1], usaPalette[0], 
+                            usaPalette[2], usaPalette[1], usaPalette[1], 
+                            usaPalette[2], usaPalette[2], usaPalette[2]];
             }
         } else if(q1Answer == "international"){
             if(q2Answer == "Africa"){
-                colorSet = [worldPalette[2], worldPalette[0], worldPalette[2], 
-                            worldPalette[0], worldPalette[1], worldPalette[0], 
-                            worldPalette[2], worldPalette[0], worldPalette[2]];
+                colorSet = [worldPalette[2], worldPalette[2], worldPalette[2], 
+                            worldPalette[1], worldPalette[1], worldPalette[1], 
+                            worldPalette[1], worldPalette[0], worldPalette[1]];
             } else if (q2Answer == "Asia"){
-                colorSet = [worldPalette[0], worldPalette[0], worldPalette[1], 
-                            worldPalette[0], worldPalette[2], worldPalette[0], 
-                            worldPalette[1], worldPalette[0], worldPalette[0]];
+                colorSet = [worldPalette[2], worldPalette[1], worldPalette[1], 
+                            worldPalette[2], worldPalette[1], worldPalette[0], 
+                            worldPalette[2], worldPalette[1], worldPalette[1]];
             } else if (q2Answer == "Europe"){
-                colorSet = [worldPalette[1], worldPalette[1], worldPalette[1], 
-                            worldPalette[0], worldPalette[0], worldPalette[0], 
-                            worldPalette[2], worldPalette[2], worldPalette[2]];
-            } else if (q2Answer == "North America"){
-                colorSet = [worldPalette[0], worldPalette[2], worldPalette[1], 
-                            worldPalette[2], worldPalette[2], worldPalette[1], 
-                            worldPalette[1], worldPalette[1], worldPalette[1]];
-            } else if (q2Answer == "Oceania"){
-                colorSet = [worldPalette[1], worldPalette[1], worldPalette[2], 
-                            worldPalette[1], worldPalette[2], worldPalette[0], 
-                            worldPalette[2], worldPalette[0], worldPalette[0]];
-            } else if (q2Answer == "South America"){
                 colorSet = [worldPalette[2], worldPalette[1], worldPalette[2], 
-                            worldPalette[2], worldPalette[0], worldPalette[2], 
+                            worldPalette[1], worldPalette[0], worldPalette[1], 
                             worldPalette[2], worldPalette[1], worldPalette[2]];
+            } else if (q2Answer == "North America"){
+                colorSet = [worldPalette[0], worldPalette[1], worldPalette[2], 
+                            worldPalette[1], worldPalette[1], worldPalette[2], 
+                            worldPalette[2], worldPalette[2], worldPalette[2]];
+            } else if (q2Answer == "Oceania"){
+                colorSet = [worldPalette[2], worldPalette[2], worldPalette[2], 
+                            worldPalette[2], worldPalette[1], worldPalette[1], 
+                            worldPalette[2], worldPalette[1], worldPalette[0]];
+            } else if (q2Answer == "South America"){
+                colorSet = [worldPalette[2], worldPalette[2], worldPalette[2], 
+                            worldPalette[1], worldPalette[1], worldPalette[2], 
+                            worldPalette[0], worldPalette[1], worldPalette[2]];
             }
         }
 
@@ -217,25 +265,61 @@ function submit(){
         tileSet[finalPosition].children[i].style.backgroundColor = colorSet[i]; 
     }
 
+    //adding chips to people who live in NYC
     if(q3Answer == "Yes"){
-        groutColor = "rgb(128, 128, 128)";
+        tileSet[finalPosition].innerHTML += chip;
+        allChips.push(chip);
     } else if (q3Answer == "No"){
-        groutColor = "rgb(209, 209, 209)"
+        allChips.push('');
     }
+
+    //adding info to new tile keys
+        if(q1Answer == "nyc"){
+            if(q3Answer == "Yes"){
+                keys[finalPosition].innerHTML = "From " + q2Answer + "<br>Lives in NYC";
+            } else if(q3Answer =="No"){
+                keys[finalPosition].innerHTML = "From " + q2Answer + "<br>Visiting NYC";
+            }
+        } else if (q1Answer == "otherUS"){
+            if(q3Answer == "Yes"){
+                keys[finalPosition].innerHTML = "From " + q2Answer + " of the US<br>Lives in NYC";
+            } else if(q3Answer =="No"){
+                keys[finalPosition].innerHTML = "From " + q2Answer + " of the US<br>Visiting NYC";
+            }
+        } else if(q1Answer == "international"){
+            if(q3Answer == "Yes"){
+                keys[finalPosition].innerHTML = "From " + q2Answer + "<br>Lives in NYC";
+            } else if(q3Answer =="No"){
+                keys[finalPosition].innerHTML = "From " + q2Answer + "<br>Visiting NYC";
+            }
+        }
+
+
+
     //  else if (q3Answer == "No"){
     //     groutColor = "white";
     // }
 
-    for(let i =0;i<9;i++){
-        tileSet[finalPosition].children[i].style.border = "1px solid " + groutColor;
-    }
+    // for(let i =0;i<9;i++){
+    //     tileSet[finalPosition].children[i].style.border = "1px solid " + groutColor;
+    // }
     
-    allGroutColors.push(groutColor);
     allColors.push(colorSet);
     allPositions.push(finalPosition);
     allQ1Answers.push(q1Answer);
     allQ2Answers.push(q2Answer);
     allQ3Answers.push(q3Answer);
+
+    keys = document.getElementsByClassName("key");
+    
+    for(let i=0;i<allPositions.length;i++){
+        keys[allPositions[i]].addEventListener("mouseover",function(){
+            keys[allPositions[i]].style.opacity = "70%";
+        })
+        keys[allPositions[i]].addEventListener("mouseout",function(){
+            keys[allPositions[i]].style.opacity = "0";
+        })
+    }
 
 
     setCookie("q1Answers", JSON.stringify(allQ1Answers), 1);
@@ -243,7 +327,7 @@ function submit(){
     setCookie("q3Answers", JSON.stringify(allQ3Answers), 1);
     setCookie("positions", JSON.stringify(allPositions), 1);
     setCookie("colors", JSON.stringify(allColors), 1);
-    setCookie("groutColors", JSON.stringify(allGroutColors), 1);
+    setCookie("chips", JSON.stringify(allChips), 1);
 
     
     //going back to q1
@@ -252,6 +336,7 @@ function submit(){
 
 }
 submitButton.addEventListener("click", submit);
+
 
 //cookie functions
 
